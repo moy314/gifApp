ebb136f5c2ba7f726bb530fa7dfd6943b7a06687
moy314 <moy5912@gmail.com>
2022-07-12 20:27:30 -0500

    3: agregando componentes busqueda y resultados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
 // import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { SharedModule } from './shared/shared.module';
 
@@ -13,7 +14,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
